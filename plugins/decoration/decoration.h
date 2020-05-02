58d711113bf9db0e51a432790925d0a8e986ed7b
rekols <rekols@foxmail.com>
2020-05-02 22:32:39 +0800

    blur the window title bar

--- plugins/decoration/decoration.h
+++ plugins/decoration/decoration.h
@@ -80,7 +80,7 @@ private:
 private:
     int m_titleBarHeight = 30;
     int m_frameRadius = 8;
-    QColor m_titleBarBgColor = QColor(255, 255, 255);
+    QColor m_titleBarBgColor = QColor(255, 255, 255, 200);
     QColor m_titleBarFgColor = QColor(51, 51, 51);
 
     QIcon m_closeIcon;
